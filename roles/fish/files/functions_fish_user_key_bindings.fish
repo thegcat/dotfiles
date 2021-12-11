72f32c057d0e4e2b41396f924fc1e86b6a5019de
Felix Schäfer <felix@thegcat.net>
2021-12-11 18:56:44 +0100

    Remove fzf

--- roles/fish/files/functions_fish_user_key_bindings.fish
+++ roles/fish/files/functions_fish_user_key_bindings.fish
@@ -2,6 +2,4 @@ function fish_user_key_bindings
   # Remap up key to first merge history to the current running shell then search
   bind \e\[A 'history merge ; up-or-search'
   bind -k up 'history merge ; up-or-search'
-
-  fzf_key_bindings
 end
