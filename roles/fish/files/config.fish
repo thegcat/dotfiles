85942259c07c02f810210bca54c6fc165181ccbf
Felix Schäfer <felix@thegcat.net>
2021-12-11 18:55:24 +0100

    Move homebrew config to separate fish snippet

--- roles/fish/files/config.fish
+++ roles/fish/files/config.fish
@@ -8,8 +8,6 @@ alias cat='bat'
 alias ls='lsd'
 alias ping='prettyping --nolegend'
 
-fish_add_path /opt/homebrew/bin
-
 set -g fish_user_paths "/opt/homebrew/opt/mysql@5.6/bin" $fish_user_paths
 set -gx LDFLAGS "-L/opt/homebrew/opt/mysql@5.6/lib"
 set -gx CPPFLAGS "-I/opt/homebrew/opt/mysql@5.6/include"
