2a3b0c1a43ba8bc22b8df7ffa0c07d78c1feec6e
Felix Schäfer <felix@thegcat.net>
2021-12-12 19:19:32 +0100

    Moved to Planio role

--- roles/fish/files/config.fish
+++ roles/fish/files/config.fish
@@ -7,7 +7,3 @@ alias g='git'
 alias cat='bat'
 alias ls='lsd'
 alias ping='prettyping --nolegend'
-
-set -g fish_user_paths "/opt/homebrew/opt/mysql@5.6/bin" $fish_user_paths
-set -gx LDFLAGS "-L/opt/homebrew/opt/mysql@5.6/lib"
-set -gx CPPFLAGS "-I/opt/homebrew/opt/mysql@5.6/include"
